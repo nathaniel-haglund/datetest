e6057ee8a90a6b02108f99a8cb69a83585540e34
Nate Haglund <nathaniel.haglund@outlook.com>
2023-05-18 16:03:05 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import ReactDatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
 
 function App() {
+const [holidays, setHolidays] = useState([])
+const [startDate, setStartDate] = useState(new Date())
+const [endDate, setEndDate] = useState(new Date())
+const [holidayToAdd, setAdd] = useState(new Date())
+
+const businessHours = () => {
+  let count = 0
+  const currentDate = new Date(startDate.getTime())
+  while (currentDate <= endDate.getTime()) {
+    const dayOfWeek = currentDate.getDay()
+    if(dayOfWeek !== 0 && dayOfWeek !== 6 && holidays.filter(day => day.getTime() === currentDate.getTime()).length === 0) {
+      count++
+    }
+    currentDate.setDate(currentDate.getDate() + 1)
+  }
+  return count * 9
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>There are {businessHours()} business hours between dates</h1>
+      <p>start date</p>
+      <ReactDatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
+      <p>end date</p>
+      <ReactDatePicker selected={endDate} onChange={(date) => setEndDate(date)} />
+      <p>Add holiday</p>
+      <ReactDatePicker selected={holidayToAdd} onChange={(date) => setAdd(date)} />
+      <button onClick={() => setHolidays([...holidays, holidayToAdd])}>Add holiday</button>
+      <p>holidays</p>
+      {holidays.map(day => <p>{`${day.getMonth()+1}/${day.getDate()}`}</p>)}
     </div>
-  );
+  )
 }
 
 export default App;
